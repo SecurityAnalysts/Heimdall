c1f9092efa7e08d0886b9ab5a76c9851659a4bc1
Ygor Simões <f1v5.exe@gmail.com>
2019-12-22 23:05:49 -0300

    Simple updates. Added Heimdall upgrade option.

--- src/help.py
+++ src/help.py
@@ -2,7 +2,7 @@
 
 Message = """
 Usage: ./heimdall.py [-h, --help] [-u, --url] [--wordlist (1, 2, 3)]
-                   [--user-agent <custom>]
+                     [--user-agent <custom>] [--update]
 
 Description: Heimdall is a project created entirely in Python to facilitate the 
              intrusion testing process on a website. Simple, powerful, fast and 
@@ -15,4 +15,5 @@ Optional Arguments:
    -u URL, --url URL      Target URL (http://www.site.com/)
    --wordlist (1, 2, 3)   Set wordlist. Default: 1 (Small) and Max: 3 (Big)
    --user-agent           Customize the User-Agent. Default: Random User-Agent
+   --update               Upgrade Heimdall to its latest available version.
 """
